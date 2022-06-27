98aa1c3a28cbecf8a543d8b2a3438687853dd0aa
Rusi Boyadjiev <rusiboyadjiev@gmail.com>
2022-06-27 13:30:44 +0300

    added new lpp balance query
    Signed-off-by: Rusi Boyadjiev <rusiboyadjiev@gmail.com>

--- src/contracts/clients/Lease.ts
+++ src/contracts/clients/Lease.ts
@@ -1,36 +1,52 @@
-import { LeaseApply, LeaserConfig, LoanInfo } from '../types';
-import { NolusClient } from '../../client';
-import { closeLeaseMsg, getCurrentOpenLeasesMsg, getLeaserConfigMsg, getLeaseStatusMsg, getLoanInformationMsg, makeLeaseApplyMsg, openLeaseMsg, repayLeaseMsg, setLeaserConfigMsg } from '../messages';
+import { LeaseApply, LeaserConfig, LoanInfo, LppBalance } from '../types';
+import {
+    closeLeaseMsg,
+    getCurrentOpenLeasesMsg,
+    getLeaserConfigMsg,
+    getLeaseStatusMsg,
+    getLoanInformationMsg,
+    getLppBalanceMsg,
+    makeLeaseApplyMsg,
+    openLeaseMsg,
+    repayLeaseMsg,
+    setLeaserConfigMsg,
+} from '../messages';
 import { NolusWallet } from '../../wallet';
 import { StdFee } from '@cosmjs/stargate';
 import { Coin } from '@cosmjs/proto-signing';
 import { ExecuteResult } from '@cosmjs/cosmwasm-stargate/build/signingcosmwasmclient';
 import { LeaseStatus } from '../types/LeaseStatus';
+import { CosmWasmClient } from '@cosmjs/cosmwasm-stargate';
 
 export class Lease {
+    private cosmWasmClient!: CosmWasmClient;
+
+    constructor(cosmWasmClient: CosmWasmClient) {
+        this.cosmWasmClient = cosmWasmClient;
+    }
+
     public async makeLeaseApply(contractAddress: string, amount: string, denom: string): Promise<LeaseApply> {
-        const cosm = await NolusClient.getInstance().getCosmWasmClient();
-        return await cosm.queryContractSmart(contractAddress, makeLeaseApplyMsg(amount, denom));
+        return await this.cosmWasmClient.queryContractSmart(contractAddress, makeLeaseApplyMsg(amount, denom));
     }
 
     public async getCurrentOpenLeases(contractAddress: string, ownerAddress: string): Promise<string[]> {
-        const cosm = await NolusClient.getInstance().getCosmWasmClient();
-        return await cosm.queryContractSmart(contractAddress, getCurrentOpenLeasesMsg(ownerAddress));
+        return await this.cosmWasmClient.queryContractSmart(contractAddress, getCurrentOpenLeasesMsg(ownerAddress));
     }
 
     public async getLeaseStatus(contractAddress: string): Promise<LeaseStatus> {
-        const cosm = await NolusClient.getInstance().getCosmWasmClient();
-        return await cosm.queryContractSmart(contractAddress, getLeaseStatusMsg());
+        return await this.cosmWasmClient.queryContractSmart(contractAddress, getLeaseStatusMsg());
     }
 
     public async getLeaserConfig(contractAddress: string): Promise<LeaserConfig> {
-        const cosm = await NolusClient.getInstance().getCosmWasmClient();
-        return await cosm.queryContractSmart(contractAddress, getLeaserConfigMsg());
+        return await this.cosmWasmClient.queryContractSmart(contractAddress, getLeaserConfigMsg());
     }
 
     public async getLoanInformation(contractAddress: string, leaseAddress: string): Promise<LoanInfo> {
-        const cosm = await NolusClient.getInstance().getCosmWasmClient();
-        return await cosm.queryContractSmart(contractAddress, getLoanInformationMsg(leaseAddress));
+        return await this.cosmWasmClient.queryContractSmart(contractAddress, getLoanInformationMsg(leaseAddress));
+    }
+
+    public async getLppBalance(contractAddress: string): Promise<LppBalance> {
+        return await this.cosmWasmClient.queryContractSmart(contractAddress, getLppBalanceMsg());
     }
 
     public async openLease(contractAddress: string, nolusWallet: NolusWallet, leaseDenom: string, fee: StdFee | 'auto' | number, fundCoin?: Coin[]): Promise<ExecuteResult> {
---
+++ src/contracts/types/LppBalance.ts
@@ -0,0 +1,7 @@
+import { Asset } from './Asset';
+
+export interface LppBalance {
+    balance: Asset;
+    total_principal_due: Asset;
+    total_interest_due: Asset;
+}
