386aa50bb4d4844681184741c569e65dc342ecf2
Alexander Makedonski <alexanderm@prime-code.com>
2023-01-11 09:20:57 +0200

    add amino types for ibc transfer

--- src/wallet/NolusWalletFactory.ts
+++ src/wallet/NolusWalletFactory.ts
@@ -1,17 +1,26 @@
 import { OfflineDirectSigner } from '@cosmjs/proto-signing';
-import { GasPrice } from '@cosmjs/stargate';
+import { createIbcAminoConverters, GasPrice, createBankAminoConverters } from '@cosmjs/stargate';
 import { NolusClient } from '../client';
 import { NolusWallet } from './NolusWallet';
 import { ChainConstants } from '../constants/';
 import { LedgerSigner } from '@cosmjs/ledger-amino';
+import { AminoTypes } from "@cosmjs/stargate";
+
+const aminoTypes = {
+    ...createIbcAminoConverters(),
+    ...createBankAminoConverters()
+}
+
+const MsgTransferAmino = new AminoTypes(aminoTypes);
 
 export const nolusOfflineSigner = async (offlineDirectSigner: OfflineDirectSigner): Promise<NolusWallet> => {
     const tendermintClient = await NolusClient.getInstance().getTendermintClient();
     const price = GasPrice.fromString(ChainConstants.GAS_PRICE);
-    return new NolusWallet(tendermintClient, offlineDirectSigner, { prefix: ChainConstants.BECH32_PREFIX_ACC_ADDR, gasPrice: price });
+    return new NolusWallet(tendermintClient, offlineDirectSigner, { prefix: ChainConstants.BECH32_PREFIX_ACC_ADDR, aminoTypes: MsgTransferAmino, gasPrice: price });
 };
 
 export const nolusLedgerWallet = async (ledgerSigner: LedgerSigner): Promise<NolusWallet> => {
     const tendermintClient = await NolusClient.getInstance().getTendermintClient();
-    return new NolusWallet(tendermintClient, ledgerSigner, { prefix: ChainConstants.BECH32_PREFIX_ACC_ADDR });
+    return new NolusWallet(tendermintClient, ledgerSigner, { prefix: ChainConstants.BECH32_PREFIX_ACC_ADDR, aminoTypes: MsgTransferAmino });
 };
+
