335064676d01a6f70d50682198f06b5753385c4c
desislavva <dessislv28th@gmail.com>
2024-01-12 16:49:08 +0200

    refactor: Lease - Opened state fields

--- src/contracts/types/OpenedLeaseInfo.ts
+++ src/contracts/types/OpenedLeaseInfo.ts
@@ -8,10 +8,10 @@ export interface OpenedLeaseInfo {
     loan_interest_rate: number;
     margin_interest_rate: number;
     principal_due: Asset;
-    previous_margin_due: Asset;
-    previous_interest_due: Asset;
-    current_margin_due: Asset;
-    current_interest_due: Asset;
+    overdue_margin: Asset;
+    overdue_interest: Asset;
+    due_margin: Asset;
+    due_interest: Asset;
     validity: string;
     in_progress?: RepaymentOngoingState | LiquidationOngoingState | CloseOngoingState;
 }
