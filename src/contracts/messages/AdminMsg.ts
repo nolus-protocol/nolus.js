80e1f90fb3879b157107bf00194bdc422ebaded6
desislavva <dessislv28th@gmail.com>
2024-02-21 11:01:33 +0200

    fix(contracts): Admin - getProtocol msg

--- src/contracts/messages/AdminMsg.ts
+++ src/contracts/messages/AdminMsg.ts
@@ -6,8 +6,6 @@ export const getProtocolsMsg = () => {
 
 export const getProtocolMsg = (protocolName: string) => {
     return {
-        protocol: {
-            protocol: protocolName,
-        },
+        protocol: protocolName,
     };
 };
