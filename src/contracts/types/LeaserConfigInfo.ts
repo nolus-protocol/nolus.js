8b2bf34c30f236f68341cba50aa53faf600e4e07
desislavva <dessislv28th@gmail.com>
2024-02-22 14:21:17 +0200

    fix(contracts): Leaser config

--- src/contracts/types/LeaserConfigInfo.ts
+++ src/contracts/types/LeaserConfigInfo.ts
@@ -5,10 +5,10 @@ export interface LeaserConfigInfo {
     lease_interest_rate_margin: number;
     lease_due_period: bigint;
     lease_position_spec: LeasePositionSpec;
-    lease_code_id: string;
-    time_alarms: string;
-    market_price_oracle: string;
-    profit: string;
-    lpp_addr: string;
-    dex: DEX;
+    lease_code_id?: string;
+    time_alarms?: string;
+    market_price_oracle?: string;
+    profit?: string;
+    lpp_addr?: string;
+    dex?: DEX;
 }
