840fef186b59e1849e55061acde996533cd3e887
desislavva <dessislv28th@gmail.com>
2025-01-09 11:29:58 +0200

    feat(contracts): Lease - lease currency info in opening state

--- src/contracts/types/OpeningLeaseInfo.ts
+++ src/contracts/types/OpeningLeaseInfo.ts
@@ -5,5 +5,6 @@ export interface OpeningLeaseInfo {
     downpayment: Asset;
     loan: Asset;
     loan_interest_rate: number;
+    currency: string;
     in_progress: string | TransferOutOngoingState | BuyAssetOngoingState;
 }
